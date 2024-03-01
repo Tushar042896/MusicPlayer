ace2f8996400991b8c2439bffcce3bedc3c7c1c9
Tushar Jha <tushartk.jha@gmail.com>
2024-03-01 13:15:16 +0530

    song chang functionaliy added

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ let progress = document.getElementById('progress');
 let song = document.getElementById('song');
 let ctrlIcon = document.getElementById('ctrlIcon');
 
+let songs = [
+    "music/1.mp3","music/2.mp3","music/3.mp3","music/4.mp3","music/5.mp3",
+    // Add more song URLs here
+];
+
+// Initialize current song index
+let currentSongIndex = 0;
+
 song.onloadedmetadata = function () {
     progress.max = song.duration;
     progress.value = song.currentTime;
@@ -37,3 +45,22 @@ progress.onchange = function () {
 
 }
 
+
+// Backward and Forward button functionality
+document.querySelector('.controls').addEventListener('click', function(event) {
+    if (event.target.classList.contains('fa-backward')) {
+        // Change to the previous song
+        currentSongIndex = (currentSongIndex - 1 + songs.length) % songs.length;
+        song.src = songs[currentSongIndex];
+        song.play();
+        ctrlIcon.classList.add("fa-pause");
+        ctrlIcon.classList.remove("fa-play");
+    } else if (event.target.classList.contains('fa-forward')) {
+        // Change to the next song
+        currentSongIndex = (currentSongIndex + 1) % songs.length;
+        song.src = songs[currentSongIndex];
+        song.play();
+        ctrlIcon.classList.add("fa-pause");
+        ctrlIcon.classList.remove("fa-play");
+    }
+});
